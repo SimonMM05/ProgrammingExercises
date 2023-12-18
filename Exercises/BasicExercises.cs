2e53d2125c51f33190a8cd26cc772ec4e33bd528
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-12-18 12:36:37 +0100

    Started with conditional exercises
    Need to present conditional exercise 11, not done needs work and use my own code.

---
+++ Exercises/BasicExercises.cs
@@ -0,0 +1,98 @@
+/*class BasicCase
+{
+    static void Main()
+    {
+        
+    }
+}
+
+Basic Exercises
+__________________
+
+Basic Exercise 4
+int x = -1+4*6;
+int y = (35+5)%7;
+int z = 14-4*6/11;
+int a = 2+15/6*1-7%2;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(a);
+*/
+
+/* Basic Exercise 5
+int number1, number2, temp;
+
+Console.WriteLine("Input the first number");
+number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+number2 = int.Parse(Console.ReadLine());
+
+temp = number1;
+number1 = number2;
+number2 = temp;
+
+Console.WriteLine("After swapping: ");
+Console.WriteLine("First number is : " + number1);
+Console.WriteLine("Second number is : " + number2);
+
+Console.ReadLine();
+*/
+
+/* Basic exercise 6
+
+int number2, number3, number6;
+
+Console.WriteLine("Input the number 2");
+number2 = int.Parse(Console.ReadLine()2);
+
+Console.WriteLine("Input the number 3");
+number3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the number 6");
+number6 = int.Parse(Console.ReadLine());
+
+int number = number2 * number3 * number6;
+
+Console.WriteLine("The number multiplied by 2, 3 and 6 is:");
+Console.WriteLine(number);
+*/
+
+/* Basic exercise 7
+
+int num1, num2;
+
+Console.WriteLine("Input the number 25");
+num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 == 25)
+{
+    Console.WriteLine("Input the number 4");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    if (num2 == 4);
+    {
+        int addition = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;
+
+        Console.WriteLine("Adding, subtracting, multiplying and dividing 25 and 4 gives:");
+        Console.WriteLine(addition + " " + " " + subtraction+  " " + " " + multiplication + " " + " " + division);
+    }
+}
+*/
+
+/* Basic Exercise 8
+Console.WriteLine("Input a number");
+int input1 = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+while (i <= 10)
+{
+    
+    Console.WriteLine(input1 + " multiplied by " + i + " is:");
+    Console.WriteLine(input1*i);
+    i++;
+}
+*/
