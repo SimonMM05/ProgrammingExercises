ce030fe529f7fee50a8bd714646b0d704379b96c
Simon Myrvig <simo008s@mail.hansenberg.dk>
2024-01-09 14:43:33 +0100

    Done with for loops exercise 6

--- Exercises/ForLoops.cs
+++ Exercises/ForLoops.cs
@@ -13,7 +13,7 @@ class ForLoops
 }
 */
 
-// For loop exercise 1
+/* For loop exercise 1
 
 using System;
 
@@ -25,5 +25,70 @@ class ForLoops
         {
             Console.WriteLine(i);
         }
+    }
+}
+*/
+
+/* For loop exercise 2
+
+using System;
+
+class ForLoops
+{
+    static int sum = 0;
+
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"{sum}");
+        Console.ReadLine();
+    }
+}
+*/
+
+/* For loop exercise 3
+
+using System;
+
+class ForLoops
+{
+    
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to find the sum of the numeric values");
+        int sum = 0;
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= input; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"{sum}");
+        Console.ReadLine();
+    }
+}
+*/
+
+// For loop exercise 6
+
+using System;
+
+class ForLoops
+{
+    static void Main()
+    {
+        Console.WriteLine("Input a number to display the multiplication table of that number.");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int product = input*i;
+            Console.WriteLine($"{input} * {i} = {product}");
+        }
+        Console.ReadLine();
     }
 }
