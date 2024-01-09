f27f1e09e86ad60ff8f551828885a01dc78aaaec
Simon Myrvig <simo008s@mail.hansenberg.dk>
2024-01-09 12:02:20 +0100

    Code for presentation done, but error when running

---
+++ BrainTwisterPresentation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class BrainTwister
+{
+    static void Main()
+    {
+        Console.WriteLine("Indtast et tal:");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        int result = Output(input);
+
+        Console.WriteLine($"Outputtet er: {result}");
+        Console.ReadLine();
+    }
+
+    static int Output(int n)
+    {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                result += i;
+            }
+            else
+            {
+                result -= i;
+            }
+        }
+
+        return result;
+    }
+}
