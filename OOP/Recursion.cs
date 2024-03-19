2ba81b822337805cf9b9e61299092a44debb044f
Simon Myrvig <simo008s@mail.hansenberg.dk>
2024-03-19 09:58:51 +0100

    Done with recursion exercise 5 & 6

--- OOP/Recursion.cs
+++ OOP/Recursion.cs
@@ -36,28 +36,27 @@ class Recursion
 {
     static void Main()
     {
-        Console.WriteLine("Enter a number to print that many numbers.");
-        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a number to print that many numbers to 1.");
+        int n = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("The numbers are: ");
-        PrintNumbers(number);
+        PrintNumbers(n);
     }
 
-    static void PrintNumbers(int number)
+    static void PrintNumbers(int n)
     {
-        if (number > 1)
+        if (n > 1)
         {
-            Console.Write(number + " ");
-            PrintNumbers(number - 1);
+            Console.Write(n + " ");
+            PrintNumbers(n - 1);
         }
-        else if (number == 1)
+        else if (n == 1)
         {
-            Console.Write(number + ".");
+            Console.Write(n + ".");
         }
     }
 }
 */
-
 
 /* REcursion exercise 3
 
@@ -67,15 +66,15 @@ class Recursion
     static void Main()
     {
         Console.WriteLine("Enter a number to calculate the sum of 1 to that number.");
-        int number = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
         
-        Console.WriteLine("The sum of the numbers are: " + PrintSum(number));
+        Console.WriteLine("The sum of the numbers are: " + PrintSum(n));
     }
-    static int PrintSum(int number)
+    static int PrintSum(int n)
     {
-        if (number > 1)
+        if (n > 1)
         {
-            return number + PrintSum(number - 1);
+            return n + PrintSum(n - 1);
         }
         else
         {
@@ -83,9 +82,9 @@ class Recursion
         }
     }
 }
-*/
+*
 
-// Recursion exercise 4: Write a program in C# Sharp to display the individual digits of a given number using recursion. 
+/* Recursion exercise 4: Write a program in C# Sharp to display the individual digits of a given number using recursion. 
 
 using System;
 
@@ -106,6 +105,80 @@ public class Program
             Recursion(i/10);
         }
         Console.Write(i % 10 + " ");
+    }
+}
+*/
+
+/* Recursion exercises 5: Write a program in C# Sharp to count the number of digits in a number using recursion. 
+
+using System;
+
+public class Recursion
+{
+    public static void Main()
+    {
+        Console.WriteLine("This program writes the number of digits in a given number.");
+        double n = Convert.ToDouble(Console.ReadLine());
+        
+        int numberOfDigits = CountDigits(n);
+        Console.WriteLine($"Number of digits in {n} is: {numberOfDigits}");
+    }
+
+    public static int CountDigits(double n)
+    {
+        if (Math.Abs(n) < 10)
+        {
+            return 1;
+        }
+        else
+        {
+            return 1 + CountDigits(n / 10);
+        }
+    }
+}
+*/
+
+// Recursion exercise 6: Write a program in C to print even or odd numbers in a given range using recursion.
+
+using System;
+
+public class Recursion
+{
+    public static void Main()
+    {
+        Console.WriteLine("This program prints the even and odd numbers from a range e.g. 1-20");
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"The even numbers are: ");
+        PrintEven(n);
+
+        Console.WriteLine($"\n\nThe odd numbers are: ");
+        PrintOdd(n);
+    }
+
+    public static void PrintEven(double n)
+    {
+        if (n <= 0)
+            return;
+
+        if (n % 2 == 0)
+        {
+            Console.Write(n + " ");
+        }
+
+        PrintEven(n - 1);
+    }
+    public static void PrintOdd(double n)
+    {
+        if (n <= 0)
+            return;
+
+        if (n % 2 != 0)
+        {
+            Console.Write(n + " ");
+        }
+
+        PrintOdd(n - 1);
     }
 }
 
